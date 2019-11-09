005d362d185ccfeba3ffb5f25f9ea96b81ad42db
Unknown <unknown@example.com>
2019-11-09 17:31:53 -0500

    Auto stash before merge of "master" and "origin/master"
    no

--- content.js
+++ content.js
@@ -13,11 +13,39 @@ function getJSON() {
     console.log(toReturn)
     return toReturn
 }
-//function generateArticles(title){//Title could be switched with a keyword list from the title/article
-  //  var url = "https://newsapi.org/v2/everything?q=" + title + "&apiKey=d943dcac77304701987917fb319681d9"
-    //var req = new Request(url)
-    //fetch(req)
-      //  .then(function(response))
-        //    console.log(response.json)
-//}
-     
+function compare(body, body2){
+    var orig = body.split(" ")
+    var comp = body2.split(" ")
+    for (var i = 0; i <orig.length; i++){
+        for (var j = 0; j<i; j++){
+            if(orig[i] == orig[j]){
+                orig.splice(i,1)
+            }
+        }
+
+    }
+
+    for (var i = 0; i <comp.length; i++){
+        for (var j = 0; j<i; j++){
+            if(comp[i] == comp[j]){
+                orig.splice(i,1)
+            }
+        }
+
+    }
+    var percent = getPercent(orig, comp)
+    return percent
+}    
+
+function getPercent(orig, comp){
+    orig.concat(comp)
+    var points = 0
+    for (var i = 0; i <orig.length; i++){
+        for (var j = 0; j<i; j++){
+            if(orig[i] == orig[j]){
+                points ++;
+                orig.splice(i,1)
+            }
+        }
+    return points/orig.length
+}
