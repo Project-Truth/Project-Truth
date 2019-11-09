b8656d58bc26f4bd4172be87c465cf3748a81e61
Matt <cataslay@gmail.com>
2019-11-09 16:59:30 -0500

    updated content.js

--- content.js
+++ content.js
@@ -1,8 +1,22 @@
-function generateArticles(title){//Title could be switched with a keyword list from the title/article
-    var url = "https://newsapi.org/v2/everything?q=" + title + "&apiKey=d943dcac77304701987917fb319681d9"
-    var req = new Request(url)
-    fetch(req)
-        .then(function(response))
-            console.log(response.json)
+"option strict";
+function getJSON(){
+    const pElements = document.getElementsByTagName("p")
+    let body
+    let toReturn
+    for(e of pElements ){body += e}
+    const header = document.getElementsByTagName("head").getElementsByTagName("title")
+    toReturn = JSON.stringify({
+        url : "none",
+        header: header,
+         body: body
+        })
+    console.log(toReturn)
 }
+//function generateArticles(title){//Title could be switched with a keyword list from the title/article
+  //  var url = "https://newsapi.org/v2/everything?q=" + title + "&apiKey=d943dcac77304701987917fb319681d9"
+    //var req = new Request(url)
+    //fetch(req)
+      //  .then(function(response))
+        //    console.log(response.json)
+//}
      
