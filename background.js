3b15e6abdb8e2fe3250907605021bfce9afd97f9
John <jreymartin21@gmail.com>
2019-11-09 18:49:29 -0500

    Moved parts of content to background
    Moved calculations to background

--- background.js
+++ background.js
@@ -11,4 +11,45 @@
     return bigJason
   }
 
+function conJSON(jason){
+    let temp = JSON.stringify(jason)
+    return temp['body'] 
+}
+function compare(body2){
+    let orig = conJSON(getJSON()).split(" ")
+    let comp = body2.split(" ")
+    for (let i = 0; i <orig.length; i++){
+        for (let j = 0; j<i; j++){
+            if(orig[i] == orig[j]){
+                orig.splice(i,1)
+            }
+        }
+
+    }
+
+    for (let i = 0; i <comp.length; i++){
+        for (let j = 0; j<i; j++){
+            if(comp[i] == comp[j]){
+                orig.splice(i,1)
+            }
+        }
+
+    }
+    let percent = getPercent(orig, comp)
+    return percent
+}    
+
+function getPercent(orig, comp){
+    orig.concat(comp)
+    let points = 0
+    for (let i = 0; i <orig.length; i++){
+        for (let j = 0; j<i; j++){
+            if(orig[i] == orig[j]){
+                points ++;
+                orig.splice(i,1)
+            }
+        }
+    return points/orig.length
+}
+
   
