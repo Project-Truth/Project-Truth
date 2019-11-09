754a4297357c018d130c3cb1b8b9a6149e15331e
John <jreymartin21@gmail.com>
2019-11-09 18:47:37 -0500

    findRelevantArticles
    give 5 of the most popular relevant articles

--- background.js
+++ background.js
@@ -3,3 +3,12 @@
       console.log("The color is green.");
     });
   });
+   
+  function findRelevantArticles(){//title can be switched with keywords 
+    let title = JSON.parse(getJSON())['head']
+    let queryURL = 'https://newsapi.org/v2/everything?q=' +title+'&sortBy=popularity&pageSize=5&apiKey=d943dcac77304701987917fb319681d9'; 
+    let bigJason = fetch(queryURL)
+    return bigJason
+  }
+
+  
