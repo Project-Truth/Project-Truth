63f7b8d248a5ff9098478b2dcab66a62f90a006b
John <jreymartin21@gmail.com>
2019-11-09 22:15:22 -0500

    Put finalPercent in background

--- background.js
+++ background.js
@@ -69,3 +69,28 @@ function getPercent(orig, comp){
     }
     return points/orig.length
 }}
+function finalPercentage(){
+    var currentPage = getJSON()
+    var articles = findRelevantArticles()
+    var sum = 0;
+    var amtSkipped = 0
+    for (let i = 0; i<articles['articles'].length; i++){
+        let tempPercent = compare(articles['articles'][i])//watch out for duplicates
+        if (tempPercent > .85){
+            amtSkipped ++;
+        }
+        else{
+            sum+=tempPercent
+        }      
+    }
+    if (amtSkipped>0){
+    var secondPage = nextPage()
+    }
+    while (amtSkipped >0){
+        sum+= compare(secondPage['articles'][amtSkipped-1])
+        amtSkipped--
+    }
+    var finalScore = sum/5
+    finalScore = finalScore*100
+    return finalScore
+}
