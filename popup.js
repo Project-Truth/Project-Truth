5f459837b11b32d188f8ac64f9c53e0396f88b78
John <jreymartin21@gmail.com>
2019-11-10 01:50:49 -0500

    fix popup

--- popup.js
+++ popup.js
@@ -5,9 +5,10 @@ function onclick() {
     await chrome.runtime.sendMessage({greeting: "getPercentage"}, function(returned){
      alert(returned) })
 }
+
+}
 if (amtSkipped>0){
     var secondPage = nextPage()
-}
 while (amtSkipped >0){
     sum += compare(secondPage['articles'][amtSkipped-1])
     amtSkipped--
