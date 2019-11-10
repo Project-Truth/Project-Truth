a2e280d12dcb071c43008783406d2ab06a9f5908
John <jreymartin21@gmail.com>
2019-11-10 07:46:07 -0500

    quick

--- website.js
+++ website.js
@@ -1,5 +1,10 @@
 //jon ur dumb
 function getQuery()
 {
-    var query = document.getElementByID("")
+    var query = document.getElementByID("myText.value")
+    return query
+}
+async query(url)
+{
+    var html = await fetch(url)
 }
