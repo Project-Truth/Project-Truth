1bcb5dffbb1f9ce63449a2022001dc01de139d57
John <jreymartin21@gmail.com>
2019-11-10 01:52:55 -0500

    Cleaned up popup because there was dead code

--- popup.js
+++ popup.js
@@ -7,12 +7,3 @@ function onclick() {
 }
 
 }
-if (amtSkipped>0){
-    var secondPage = nextPage()
-while (amtSkipped >0){
-    sum += compare(secondPage['articles'][amtSkipped-1])
-    amtSkipped--
-}
-var finalScore = sum/5
-finalScore = finalScore*100
-},false)
