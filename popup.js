39dc38325135e5f3e7189cff50768a205262b1e0
Matt <cataslay@gmail.com>
2019-11-10 03:07:26 -0500

    Auto stash before merge of "master" and "origin/master"

--- popup.js
+++ popup.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded',async function localFunc(){
 document.querySelector('button').addEventListener('click',onclick, false)
  async function onclick() {
     chrome.runtime.sendMessage({greeting: "getPercentage"}, function(returned){
-        console.log(returned)
+        alert(returned)
      })
 }
 
