91e97c0ec1bbafa422e95c611dd3a70b7a33b020
Matt <cataslay@gmail.com>
2019-11-10 06:11:50 -0500

    Got rid of double run bug

--- popup.js
+++ popup.js
@@ -1,10 +1,8 @@
 //alert("Popup running")
-document.addEventListener('DOMContentLoaded',async function localFunc(){
+
 document.querySelector('button').addEventListener('click',onclick, false)
  async function onclick() {
-    chrome.runtime.sendMessage({greeting: "getP"}, function(returned){
+    chrome.runtime.sendMessage({greeting: "getPercentage"}, function(returned){
         alert("Returned Data: " + returned.data)
      })
 }
-
-},false)
