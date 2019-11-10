e0a475e862f3a674ec1a22f6cfd5aee71d811f08
Matt <cataslay@gmail.com>
2019-11-10 05:53:07 -0500

    push for bernie

--- content.js
+++ content.js
@@ -1,5 +1,6 @@
 
  function getJSON() {
+     alert("getJSON in content has been called")
     const pElements = document.getElementsByTagName("p")
     let body
     let toReturn
@@ -10,12 +11,13 @@
         header: header,
          body: body
         })
-    console.log(toReturn)
+        console.log("What matt wants to Return:"+toReturn)
     return toReturn
 }
 
 chrome.runtime.onMessage.addListener( function(request,sender,sendResponse)
 {
+    alert("content.js has been called")
     console.log(sender.tab ?
         "from a content script:" + sender.tab.url :
         "from the extension");
