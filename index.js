d544aac0b7829a510219e8c3419e21d2dded484f
John <jreymartin21@gmail.com>
2019-11-10 09:31:44 -0500

    testing button click

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ async function finalPercentage(){
     alert( "finalScore:" +finalScore)
     return finalScore
 }
-btn.onClick = finalPercentage(){
+btn.onClick = "finalPercentage();"{
     console.log("click")
     document.getElementById('p').innerHTML = finalPercentage()
 }
