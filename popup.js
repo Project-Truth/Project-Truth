3b046453dcc68597286b30e3de8793e84b328e38
Matt <cataslay@gmail.com>
2019-11-10 01:58:43 -0500

    I forgot what i did but i hope it fixed it

--- popup.js
+++ popup.js
@@ -1,8 +1,8 @@
 //alert("Popup running")
 document.addEventListener('DOMContentLoaded',function (){
-document.querySelector('button').addEventListener('click', onclick, false)
-function onclick() {
-    await chrome.runtime.sendMessage({greeting: "getPercentage"}, function(returned){
+document.querySelector('button').addEventListener('click',await onclick, false)
+ async function onclick() {
+    chrome.runtime.sendMessage({greeting: "getPercentage"}, function(returned){
      alert(returned) })
 }
 if (amtSkipped>0){
